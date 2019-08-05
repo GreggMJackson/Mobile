53de61fa070b96ed672beba0be38f4c4a6eb007e
greggmjackson <greggmjackson@gmail.com>
2019-08-05 17:00:53 +0100

    Add project files.

---
+++ Icarus/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Icarus.Controllers {
+    public class HomeController : Controller {
+
+        public ActionResult Index() => View();
+        public ActionResult About() => View();
+        public ActionResult Contact() => View();
+
+        public ActionResult Javascript() => View("Javascript");
+        public ActionResult Mobile() => View("Mobile");
+        public ActionResult Cloud() => View("Cloud");
+        public ActionResult BigData() => View("BigData");
+        public ActionResult IoT() => View("IoT");
+        public ActionResult BlockChain() => View("BlockChain");
+        public ActionResult AI() => View("AI");
+
+    }
+}
